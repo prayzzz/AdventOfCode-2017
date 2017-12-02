f3883250a581a95d8c414e06bd863e20d5620368
prayzzz <patrick.bachmann@outlook.com>
2017-12-02 15:10:27 +0100

    - initial commit

---
+++ Day01.cs
@@ -0,0 +1,130 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prayzzz.Common.Unit;
+
+namespace AdventOfCode2017
+{
+    [TestClass]
+    public class Dec01Part1
+    {
+        private const char ZeroChar = '0';
+
+        [TestMethod]
+        public void Test1()
+        {
+            var solve = Solve("1122");
+            Assert.AreEqual(3, solve);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var solve = Solve("1111");
+            Assert.AreEqual(4, solve);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var solve = Solve("1234");
+            Assert.AreEqual(0, solve);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var solve = Solve("91212129");
+            Assert.AreEqual(9, solve);
+        }
+
+        [TestMethod]
+        public void InverseCaptcha()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day01.txt");
+            Console.WriteLine(Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            input = input.Trim();
+
+            var sum = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[(i + 1) % input.Length])
+                {
+                    sum += input[i] - ZeroChar;
+                }
+            }
+            
+            return sum;
+        }
+    }
+
+    [TestClass]
+    public class Dec01Part2
+    {
+        private const char ZeroChar = '0';
+
+        [TestMethod]
+        public void Test1()
+        {
+            var solve = Solve("1212");
+            Assert.AreEqual(6, solve);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var solve = Solve("1221");
+            Assert.AreEqual(0, solve);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var solve = Solve("123425");
+            Assert.AreEqual(4, solve);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var solve = Solve("123123");
+            Assert.AreEqual(12, solve);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            var solve = Solve("12131415");
+            Assert.AreEqual(4, solve);
+        }
+
+        [TestMethod]
+        public void InverseCaptcha()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day01.txt");
+            Console.WriteLine(Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            input = input.Trim();
+
+            var sum = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var halfwayIndex = (i + input.Length / 2) % input.Length;
+                if (input[i] == input[halfwayIndex])
+                {
+                    sum += input[i] - ZeroChar;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Day02.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prayzzz.Common.Unit;
+
+namespace AdventOfCode2017
+{
+    [TestClass]
+    public class Dec02Part1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            const string value = @"5 1 9 5
+                                   7 5 3
+                                   2 4 6 8";
+
+            Assert.AreEqual(18, Solve(value));
+        }
+
+        [TestMethod]
+        public void CorruptionChecksum()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day02.txt");
+            Console.WriteLine(Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            var lines = input.Trim().Split("\n");
+
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var values = line.Trim().Split(null);
+
+                var high = int.MinValue;
+                var low = int.MaxValue;
+                foreach (var valueStr in values)
+                {
+                    var value = int.Parse(valueStr);
+
+                    if (value > high)
+                    {
+                        high = value;
+                    }
+
+                    if (value < low)
+                    {
+                        low = value;
+                    }
+                }
+
+                sum += high - low;
+            }
+
+            return sum;
+        }
+    }
+
+    [TestClass]
+    public class Dec02Part2
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            const string value = @"5 9 2 8
+                                   9 4 7 3
+                                   3 8 6 5";
+
+            Assert.AreEqual(9, Solve(value));
+        }
+
+        [TestMethod]
+        public void CorruptionChecksum()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day02.txt");
+            Console.WriteLine(Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            var lines = input.Trim().Split("\n");
+
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var values = line.Trim().Split(null).Select(int.Parse).ToList();
+
+                for (var i = 0; i < values.Count; i++)
+                {
+                    for (var j = i + 1; j < values.Count; j++)
+                    {
+                        var v1 = values[i];
+                        var v2 = values[j];
+
+                        if (v2 > v1)
+                        {
+                            var temp = v1;
+                            v1 = v2;
+                            v2 = temp;
+                        }
+
+                        var result = v1 / (double) v2;
+                        if (result > 0 && result % 1 == 0)
+                        {
+                            sum += (int) result;
+                            goto LineDone;
+                        }
+                    }
+                }
+
+                LineDone:
+                ;
+            }
+
+            return sum;
+        }
+    }
+}
