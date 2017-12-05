6dbbf96a21d0df8f45a060ab6dd7bf8b525d94f3
prayzzz <patrick.bachmann@outlook.com>
2017-12-05 17:51:44 +0100

    - day05

---
+++ Day05.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prayzzz.Common.Unit;
+
+namespace AdventOfCode2017
+{
+    [TestClass]
+    public class Day05Part1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(5, Solve(@"0
+                                       3
+                                       0
+                                       1
+                                       -3"));
+        }
+
+        [TestMethod]
+        public void TwistyTrampolines()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day05.txt");
+            Console.WriteLine("Day05Part1: " + Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            var jumps = input.Trim().Split("\n").Select(int.Parse).ToArray();
+
+            var jumpCount = 0;
+            var i = 0;
+            for (;;)
+            {
+                if (i >= jumps.Length)
+                {
+                    break;
+                }
+
+                var jumpWidth = jumps[i];
+                jumps[i]++;
+
+                jumpCount++;
+                i += jumpWidth;
+            }
+
+            return jumpCount;
+        }
+    }
+
+    [TestClass]
+    public class Day05Part2
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(10, Solve(@"0
+                                        3
+                                        0
+                                        1
+                                       -3"));
+        }
+
+        [TestMethod]
+        public void TwistyTrampolines()
+        {
+            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day05.txt");
+            Console.WriteLine("Day05Part2: " + Solve(input));
+        }
+
+        private static int Solve(string input)
+        {
+            var jumps = input.Trim().Split("\n").Select(int.Parse).ToArray();
+
+            var jumpCount = 0;
+            var i = 0;
+            for (;;)
+            {
+                if (i >= jumps.Length)
+                {
+                    break;
+                }
+
+                var jumpWidth = jumps[i];
+                jumps[i] += jumpWidth >= 3 ? -1 : 1;
+                
+                jumpCount++;
+                i += jumpWidth;
+            }
+
+            return jumpCount;
+        }
+    }
+}
