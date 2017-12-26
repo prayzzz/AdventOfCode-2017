2d892a4de28da4c17607f275f977ea54e125aa88
prayzzz <prayzzz@outlook.com>
2017-12-26 10:02:18 +0100

    - day23

--- Day23.cs
+++ Day23.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using prayzzz.Common.Unit;
 
@@ -79,70 +80,43 @@ namespace AdventOfCode2017
     [TestClass]
     public class Day23Part2
     {
-        [Ignore]
         [TestMethod]
         public void CoprocessorConflagration()
         {
-            var input = TestHelper.ReadEmbeddedFile(GetType().Assembly, "Input.Day23.txt");
-            var result = Solve(input);
-            Console.WriteLine("Day23Part2: " + result);
-            Assert.AreEqual(5929, result);
+            Assert.AreEqual(907, Solve(107900, 124900));
         }
 
-        private long Solve(string input)
+        /// <summary>
+        /// Counts all non-prime numbers between <paramref name="lowerLimit"/> and <paramref name="upperLimit"/>
+        /// </summary>
+        private static long Solve(int lowerLimit, int upperLimit)
         {
-            var lines = input.Trim().Split("\n");
+            var b = lowerLimit;
 
-            var registers = new Dictionary<char, long>
+            // register['h']
+            var nonPrimeCount = 0;
+
+            // loop 03
+            while (true)
             {
-                ['a'] = 1,
-                ['b'] = 0,
-                ['c'] = 0,
-                ['d'] = 0,
-                ['e'] = 0,
-                ['f'] = 0,
-                ['g'] = 0,
-                ['h'] = 0
-            };
-
-            for (var i = 0L; i < lines.Length; i++)
-            {
-                var instructions = lines[i].Trim().Split();
-
-                switch (instructions[0])
+                // optimised inner loops
+                for (var d = 2; d < b / 2; d++)
                 {
-                    case "set":
-                        registers[instructions[1][0]] = ParseOrGetRegister(instructions[2], registers);
+                    if (b % d == 0)
+                    {
+                        nonPrimeCount += 1;
                         break;
-
-                    case "sub":
-                        registers[instructions[1][0]] -= ParseOrGetRegister(instructions[2], registers);
-                        break;
-
-                    case "mul":
-                        registers[instructions[1][0]] *= ParseOrGetRegister(instructions[2], registers);
-                        break;
-
-                    case "jnz":
-                        if (ParseOrGetRegister(instructions[1], registers) != 0)
-                        {
-                            i += ParseOrGetRegister(instructions[2], registers) - 1;
-                        }
-                        break;
+                    }
                 }
+
+                // check before increment!
+                if (b == upperLimit)
+                {
+                    return nonPrimeCount;
+                }
+
+                b += 17;
             }
-
-            return registers['h'];
-        }
-
-        private static long ParseOrGetRegister(string instructions, IReadOnlyDictionary<char, long> registers)
-        {
-            if (!long.TryParse(instructions, out var val))
-            {
-                val = registers[instructions[0]];
-            }
-
-            return val;
         }
     }
 }
